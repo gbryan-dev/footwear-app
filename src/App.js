5706f3edfefc0d717bafde1cde170e808f186260
Bryan Galamgam <62716979+gbryan-dev@users.noreply.github.com>
2023-02-15 02:22:10 -0800

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,20 +1,47 @@
 import "./App.css";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 // import Categories from "./COMPONENT/Categories";
 import AboutUs from "./COMPONENT/AboutUs";
 import Header from "./COMPONENT/Header";
 import Footer from "./COMPONENT/Footer";
 import Home from "./COMPONENT/Home";
 import Products from "./COMPONENT/Products";
+import { useState, useEffect, useRef, createContext } from "react";
+import Profile from "./COMPONENT/Profile";
+import ViewProduct from "./COMPONENT/ViewProduct";
+import SignInUp from "./COMPONENT/SignInUp";
+
+export const context = createContext();
 
 function App() {
+  const topRef = useRef();
+  const location = useLocation();
+
+  useEffect(() => {
+    localStorage.getItem("Email");
+    topRef.current.scrollIntoView();
+  }, [location]);
+
   return (
     <>
+      <div
+        ref={topRef}
+        style={{
+          height: "10px",
+          width: "100%",
+          position: "absolute",
+          left: "0px",
+          top: "0px",
+        }}
+      ></div>
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/About" element={<AboutUs />} />
         <Route path="/Products" element={<Products />} />
+        <Route path="/Profile" element={<Profile />} />
+        <Route path="/ViewProduct" element={<ViewProduct />} />
+        <Route path="/SignInUp" element={<SignInUp />} />
       </Routes>
       <Footer />
     </>
---
+++ src/COMPONENT/PRODUCTS/AllProducts.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+
+const AllProducts = [
+  {
+    id: 0,
+    img: require("./FEATURED/F1.png"),
+    price: "2529.25",
+    name: "Zoom Panda Turbo",
+    type: "Shoes",
+    qty: 1,
+    wish: false,
+  },
+  {
+    id: 1,
+    img: require("./FEATURED/F2.png"),
+    price: "2647.56",
+    name: "Element Strong 55",
+    type: "Shoes",
+    qty: 1,
+    wish: false,
+  },
+  {
+    id: 2,
+    img: require("./FEATURED/F3.png"),
+    price: "2355.42",
+    name: "Air Force 1 LV8",
+    type: "Shoes",
+    qty: 1,
+    wish: false,
+  },
+  {
+    id: 3,
+    img: require("./FEATURED/F4.png"),
+    price: "2482.97",
+    name: "Air Max Triax 96",
+    type: "Shoes",
+    qty: 1,
+    wish: false,
+  },
+  {
+    id: 4,
+    img: require("./FEATURED/F5.png"),
+    price: "2649.32",
+    name: "Air Jordan 14",
+    type: "Shoes",
+    qty: 1,
+    wish: false,
+  },
+  {
+    id: 5,
+    img: require("./FEATURED/F6.png"),
+    price: "2874.48",
+    name: "SB Zoom Janoski RM",
+    type: "Shoes",
+    qty: 1,
+    wish: false,
+  },
+];
+
+export default AllProducts;
